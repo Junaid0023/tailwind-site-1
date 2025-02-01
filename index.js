addf667f38c2be5433734da0e15abecbac7c0276
ishaq iddriss <ishaqidris95@gmail.com>
2025-02-01 22:41:58 +0000

    more

--- index.js
+++ index.js
@@ -5,19 +5,8 @@ const navBtn = document.querySelector(".navBtn");
 
 navBtn.addEventListener('click', ()=>{
     navlinks.classList.toggle('hidden')
-    navlinks.classList.toggle('transform-translate-y-full')
-
    
-   
-    if(navBtn.classList.contains('fa-bars')){
-        navBtn.classList.remove('fa-bars')
-        navBtn.classList.add('<fa-x') 
-    }else{
-        navBtn.classList.remove('fa-x');
-        navBtn.classList.add('fa-bars');
-    }
 
-    
 
 })
 
