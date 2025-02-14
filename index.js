eb70d3726f6f1d36315b4c59cc53ebc26b887e86
ishaq iddriss <ishaqidris95@gmail.com>
2025-02-14 22:34:15 +0000

    swiper js added

--- index.js
+++ index.js
@@ -10,5 +10,41 @@ navBtn.addEventListener('click', ()=>{
 
 })
 
+
+// ============SWIPJS-----------------
+
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+      loop: true,
+      spaceBetween: 20,
+      slidesPerView: 1, // Default for small screens
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      autoplay: {
+        delay: 3000,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2, // Show 2 slides on screens ≥ 640px
+        },
+        1024: {
+          slidesPerView: 3, // Show 3 slides on screens ≥ 1024px
+        },
+      },
+    });
+  });
+
+
+
+
+
+// =============COPYRIGHT YEAR ====================
+
 document.getElementById('year').textContent = new Date().getFullYear();
 
